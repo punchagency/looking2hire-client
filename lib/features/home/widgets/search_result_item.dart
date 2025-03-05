69e3dd96ff540cb06574229b3a665f5e88b6dd88
ayomi <ayomilotunde02@gmail.com>
2025-03-05 11:23:21 +0100

    fixes

--- lib/features/home/widgets/search_result_item.dart
+++ lib/features/home/widgets/search_result_item.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:looking2hire/app_colors.dart';
 import 'package:looking2hire/features/home/models/job.dart';
